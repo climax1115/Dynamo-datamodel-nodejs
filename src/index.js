42fc3339bf32ef10136abfbd3f4e31cd2d79c615
Kevin Edward <climax11150102.cmx@gmail.com>
2022-03-19 04:22:39 -0700

    latest update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 
-import Calculator from "./Calculator";
-import "./index.css";
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
-const projectName = "javaScript-calculator";
-localStorage.setItem("project-name", projectName);
-
-ReactDOM.render(<Calculator />, document.getElementById("root"));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
